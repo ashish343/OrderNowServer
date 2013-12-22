71b1c3ba7027a0a476eb179c3bc5857d6c53fe59
ashish343 <singhalashish343@gmail.com>
2013-12-22 17:04:11 +0530

    Removing un-necessary files.

--- src/main/webapp/resources/js/bodyJS.js
+++ src/main/webapp/resources/js/bodyJS.js
@@ -6,21 +6,25 @@ P.when('jQuery').execute(function($) {
 var myFunc = function($) {
 	$("#action-home").click(function(e) {
 		$('.carousel').carousel(0);
+		$('.carousel').carousel('pause');
 		e.preventDefault();
 	});
 
 	$("#action-app").click(function(e) {
 		$('.carousel').carousel(1);
+		$('.carousel').carousel('pause');
 		e.preventDefault();
 	});
 
 	$("#action-blog").click(function(e) {
 		$('.carousel').carousel(2);
+		$('.carousel').carousel('pause');
 		e.preventDefault();
 	});
 
 	$("#action-sign-in").click(function(e) {
 		$('.carousel').carousel(3);
+		$('.carousel').carousel('pause');
 		e.preventDefault();
 	});
 }
